52431ff6ad2b8fdd58c4ecdcb3509801f082067e
rodrigo.baggio.si@gmail.com <Baggio25>
2023-01-25 11:22:30 -0300

    Aula 3.14

--- algafood-api/src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
@@ -6,6 +6,7 @@ import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 public class AlteracaoCozinhaMain {
 
@@ -14,10 +15,10 @@ public class AlteracaoCozinhaMain {
 				.web(WebApplicationType.NONE)
 				.run(args);
 		
-		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		CozinhaRepository cozinhaRepository = applicationContext.getBean(CozinhaRepository.class);
 		
 		Cozinha cozinha = new Cozinha(1L, "Brasileira");
-		cadastroCozinha.salvar(cozinha);
+		cozinhaRepository.salvar(cozinha);
 	}
 
 }
--- algafood-api/src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
@@ -8,6 +8,7 @@ import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 public class ConsultaCozinhaMain {
 
@@ -16,8 +17,8 @@ public class ConsultaCozinhaMain {
 				.web(WebApplicationType.NONE)
 				.run(args);
 		
-		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
-		List<Cozinha> list = cadastroCozinha.listar();
+		CozinhaRepository cozinhaRepository = applicationContext.getBean(CozinhaRepository.class);
+		List<Cozinha> list = cozinhaRepository.listar();
 		
 		for(Cozinha cozinha : list) {
 			System.out.println(cozinha.getNome());
