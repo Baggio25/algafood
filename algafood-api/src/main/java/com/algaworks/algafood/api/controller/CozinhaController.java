b87bc38eaed43b32f5a9192e93bab8f6d51c8856
rodrigo.baggio.si@gmail.com <Baggio25>
2023-01-25 16:51:04 -0300

    Aula 4.20 e 4.21

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,20 @@ public class CozinhaController {
 	private CozinhaRepository cozinhaRepository;
 	
 	@GetMapping
-	public List<Cozinha> listar() {
-		return cozinhaRepository.findAll();
+	public ResponseEntity<List<Cozinha>> listar() {
+		List<Cozinha> lista = cozinhaRepository.findAll(); 
+		return ResponseEntity.ok(lista);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+		Cozinha cozinha = cozinhaRepository.findById(id).get();
+		
+		if(cozinha != null) {
+			return ResponseEntity.ok(cozinha);	
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 }
