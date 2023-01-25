4ed8de6e8ef259b04b4710336e3ce84e365e577a
rodrigo.baggio.si@gmail.com <Baggio25>
2023-01-25 14:41:29 -0300

    Aula 3.20

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Permissao.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Permissao.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,24 +12,17 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@Table(name = "cozinha")
-public class Cozinha {
+public class Permissao {
 
-	@EqualsAndHashCode.Include
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(nullable = false)
-	private String nome;
-	
-	public Cozinha() {
-
-	}
-
-	public Cozinha(Long id, String nome) {
-		this.id = id;
-		this.nome = nome;
-	}
-	
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String nome;
+    
+    @Column(nullable = false)
+    private String descricao;
+    
 }
