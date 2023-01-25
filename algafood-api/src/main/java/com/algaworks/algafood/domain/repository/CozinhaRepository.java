72df74f15aa5d8c800191bb2e2cf4b9870d28fad
rodrigo.baggio.si@gmail.com <Baggio25>
2023-01-25 15:50:48 -0300

    Aulas 4.10 e 4.11

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,14 +1,10 @@
 package com.algaworks.algafood.domain.repository;
 
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
-public interface CozinhaRepository {
-
-	List<Cozinha> listar();
-	Cozinha buscar(Long id);
-	Cozinha salvar(Cozinha cozinha);
-	void remover(Cozinha cozinha);
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{
+	
 	
 }
