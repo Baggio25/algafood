23c641fc55c5a32bbfeb7aedc7f7dfbd048e25eb
rodrigo.baggio.si@gmail.com <Baggio25>
2023-01-25 11:00:07 -0300

    Aulas 3.6 a 3.12

---
+++ algafood-api/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO cozinha (nome) VALUES ("Italiana");
+INSERT INTO cozinha (nome) VALUES ("Indiana");
+INSERT INTO cozinha (nome) VALUES ("Tailandesa");
+INSERT INTO cozinha (nome) VALUES ("Japonesa");
