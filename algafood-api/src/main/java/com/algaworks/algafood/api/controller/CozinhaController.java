550e69a1c4cc8045e852d9e19d61f936ebd38506
rodrigo.baggio.si@gmail.com <Baggio25>
2023-01-27 17:41:26 -0300

    Aula 5.6

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +37,11 @@ public class CozinhaController {
 	@GetMapping
 	public List<Cozinha> listar() {
 		return cozinhaRepository.findAll();
+	}
+	
+	@GetMapping("/por-nome")
+	public List<Cozinha> listarPorNome(@RequestParam("nome") String nome) {
+		return cozinhaRepository.findByNome(nome);
 	}
 	
 	@GetMapping("/{id}")
