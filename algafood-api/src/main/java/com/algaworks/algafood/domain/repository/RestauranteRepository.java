bea8408d78e7abc709e74fd22eb079ec1fa43664
rodrigo.baggio.si@gmail.com <Baggio25>
2023-01-30 09:35:16 -0300

    Aula 5.10

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -15,9 +14,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
 	
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
-	@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> buscarPorNome(String nome, @Param("id") Long cozinhaId);
-	
-	//List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 	
 }
