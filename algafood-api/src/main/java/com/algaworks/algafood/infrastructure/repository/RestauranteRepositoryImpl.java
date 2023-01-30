67ec1d8dc249090efbb74a01c18a3140a5f17298
rodrigo.baggio.si@gmail.com <Baggio25>
2023-01-30 11:21:04 -0300

    Aulas 5.13, 5.14 e 5.15

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,17 @@
 package com.algaworks.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -22,6 +27,38 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal){
+		return queryComCriteria(nome, taxaInicial, taxaFinal);		
+	}
+	
+	//Exemplo com criteria
+	private List<Restaurante> queryComCriteria(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal) {
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		
+		var predicates = new ArrayList<Predicate>();
+		if(StringUtils.hasText(nome)) {		
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));	
+		}
+		
+		if(taxaInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaInicial));
+		}
+		
+		if(taxaFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFinal));			
+		}
+		
+		criteria.where(predicates.toArray(new Predicate[0]));		
+		
+		TypedQuery<Restaurante> query = entityManager.createQuery(criteria);
+		return query.getResultList();
+	}
+
+	//Exemplo com query JPQL
+	@SuppressWarnings("unused")
+	private List<Restaurante> queryJpql(String nome, BigDecimal taxaInicial, BigDecimal taxaFinal) {
 		var jpql = new StringBuilder();
 		jpql.append("from Restaurante where 0 = 0 ");
 		
@@ -46,7 +83,7 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 				.createQuery(jpql.toString(), Restaurante.class);
 		
 		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
-		return query.getResultList();		
+		return query.getResultList();
 	}
 	
 }
