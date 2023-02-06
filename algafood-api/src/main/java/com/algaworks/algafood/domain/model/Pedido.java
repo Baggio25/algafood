97027bf99c552a354a7050ade3b27691ebf8350c
rodrigo.baggio.si@gmail.com <Baggio25>
2023-02-06 08:49:29 -0300

    Aula 7.12

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -0,0 +1,77 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private BigDecimal subtotal;	
+	
+	@Column(name = "taxa_frete")
+	private BigDecimal taxaFrete;
+	
+	@Column(name = "valor_total")
+	private BigDecimal valorTotat;
+	
+	@CreationTimestamp
+	@Column(name = "data_criacao")
+	private LocalDateTime dataCriacao;
+	
+	@Column(name = "data_confirmacao")
+	private LocalDateTime dataConfirmacao;
+	
+	@Column(name = "data_entrega")
+	private LocalDateTime dataEntrega;
+	
+	@Column(name = "data_cancelamento")
+	private LocalDateTime dataCancelamento;
+	
+	@Embedded
+	private Endereco enderecoEntrega;
+	
+	private StatusPedido status;
+	
+	@ManyToOne
+	@JoinColumn(name = "forma_pagamento_id", nullable = false)
+	private FormaPagamento formaPagamento;
+	
+	@ManyToOne
+	@JoinColumn(name = "restaurante_id", nullable = false)
+	private Restaurante restaurante;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id", nullable = false)
+	private Usuario cliente;
+	
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens = new ArrayList<>();
+	
+	
+}
