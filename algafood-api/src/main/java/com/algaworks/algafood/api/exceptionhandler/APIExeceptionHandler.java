fdc8d1ef396be36702e27dd1d142a0166ca5ce7a
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-02-06 21:51:28 -0300

    Aula 8.15

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/APIExeceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/APIExeceptionHandler.java
@@ -4,16 +4,16 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 
 @ControllerAdvice
-public class APIExeceptionHandler {
+public class APIExeceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
 	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e){
@@ -34,17 +34,6 @@ public class APIExeceptionHandler {
 				.build();
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(erro);
-	}
-	
-	@ExceptionHandler(HttpMediaTypeException.class)
-	public ResponseEntity<?> tratarHttpMediaTypeException(){
-		Erro erro = Erro.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem("O tipo de mídia não é aceito")
-				.build();
-		
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
 				.body(erro);
 	}
 	
