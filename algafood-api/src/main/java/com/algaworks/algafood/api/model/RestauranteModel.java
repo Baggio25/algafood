31fedb2e2ca93b1a2513eb4417421758b431b539
rodrigo.baggio.si@gmail.com <Baggio25>
2023-02-16 11:32:12 -0300

    Aula 12.6

--- algafood-api/src/main/java/com/algaworks/algafood/api/model/RestauranteModel.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/RestauranteModel.java
@@ -14,6 +14,7 @@ public class RestauranteModel {
 	private String nome;
 	private BigDecimal taxaFrete;
 	private Boolean ativo;
-	
+
+	private EnderecoModel endereco;
 	private CozinhaModel cozinha; 
 }
