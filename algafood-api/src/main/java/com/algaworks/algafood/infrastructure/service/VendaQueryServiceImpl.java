b217d2d604500ae1b16201bff4ba4cb31b9463f5
rodrigo.baggio.si@gmail.com <Baggio25>
2023-02-28 15:05:12 -0300

    Aula 13.15

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,15 +1,18 @@
 package com.algaworks.algafood.infrastructure.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
 
@@ -20,12 +23,15 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 	private EntityManager manager;	
 	
 	/**
-	 * Em SQL Ficaria:
+	 * Com SQL Ficaria:
 	 * 
 	 *  SELECT DATE(p.data_criacao) AS data_criacao,
 	 *  	   COUNT(p.id) AS total_vendas,
 	 *  	   SUM(p.valor_total) as total_faturado
 	 *  FROM pedido p
+	 *  WHERE
+	 *  	   restaurante_id = :restauranteId AND
+	 *  	   data_criacao between :dataCriacaoInicio AND :dataCriacaoFim  
 	 *  GROUP BY DATE(p.data_criacao)
 	 */
 	@Override
@@ -33,6 +39,7 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 		var builder = manager.getCriteriaBuilder();
 		var query = builder.createQuery(VendaDiaria.class); //Define o tipo de retorno da query
 		var root = query.from(Pedido.class);
+		var predicates = new ArrayList<Predicate>();
 		
 		var functionDateDataCriacao = builder.function("date", Date.class, root.get("dataCriacao"));
 		
@@ -41,6 +48,21 @@ public class VendaQueryServiceImpl implements VendaQueryService{
 				builder.count(root.get("id")),
 				builder.sum(root.get("valorTotal")));
 		
+		if(vendaDiariaFilter.getRestauranteId() != null) {
+			predicates.add(builder.equal(root.get("restaurante"), vendaDiariaFilter.getRestauranteId()));
+		}
+		
+		if(vendaDiariaFilter.getDataCriacaoInicio() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), vendaDiariaFilter.getDataCriacaoInicio()));
+		}
+		
+		if(vendaDiariaFilter.getDataCriacaoFim() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), vendaDiariaFilter.getDataCriacaoFim()));
+		}
+		
+		predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+		
+		query.where(predicates.toArray(new Predicate[0]));
 		query.select(selection);
 		query.groupBy(functionDateDataCriacao);
 		
