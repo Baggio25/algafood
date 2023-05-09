ea4362ed47fc88715f3cdb9d68e038a9e6ae516c
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-09 16:39:20 -0300

    16.8. Implementando um formulário de cadastro e fazendo requisição POST com JavaScript

--- algafood-js-client/FormaPagamentoService.js
+++ algafood-js-client/FormaPagamentoService.js
@@ -9,6 +9,35 @@ function consultarFormasPagamento() {
     });
 };
 
+function cadastrarFormasPagamento() {
+    var formaPagamentojSON = JSON.stringify({
+        "descricao": $("#campo-descricao").val()
+    })
+
+    $.ajax({
+        url: "http://localhost:8080/formas-pagamento",
+        type: "POST",
+        data: formaPagamentojSON,
+        contentType: "application/json",
+
+        success: function(response) {
+            alert("Forma de Pagamento " + response.id +" - " + response.descricao + " adicionada!");
+            consultarFormasPagamento();
+            $("#campo-descricao").val("");
+        },
+
+        error: function(error) {
+            if(error.status === 400) {
+                var problem = JSON.parse(error.responseText);
+                alert(problem.userMessage);
+            }else {
+                alert("Erro ao cadastrar forma de pagamento!");
+            }
+        }
+    });
+};
+
+
 function preencherTabela(formasPagamento) {
     $("#tabela tbody tr").remove();
   
@@ -27,3 +56,4 @@ function preencherTabela(formasPagamento) {
 
 
 $("#botaoConsultar").click(consultarFormasPagamento);
+$("#botaoCadastrar").click(cadastrarFormasPagamento);
