a721f4719bb11bee8972ab9246bb6dc2626083a5
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-10 10:49:43 -0300

    16.10. Implementando um client da REST API com Java e Spring (RestTemplate)

---
+++ algafood-java-client/src/main/java/com/algaworks/algafood/client/ListagemRestaurantesMain.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.client;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.algaworks.algafood.client.api.RestauranteClient;
+
+public class ListagemRestaurantesMain {
+
+	public static void main(String[] args) {
+		RestTemplate restTemplate = new RestTemplate();
+		RestauranteClient restauranteClient = new RestauranteClient("http://localhost:8080", restTemplate);
+		
+		restauranteClient.listar().stream().forEach(
+				(restaurante) -> System.out.println("----- Restaurante: " + restaurante.getNome() + ","
+						+ " Taxa Frete: " + restaurante.getTaxaFrete() + ", "
+						+ " Cozinha: " + restaurante.getCozinha().getNome())
+		);
+	}
+	
+}
